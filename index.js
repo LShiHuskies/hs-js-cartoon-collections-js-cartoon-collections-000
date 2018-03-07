9e39936f186a56384c1ef7470b6bb5d00279daa7
Louis Shi <louis.shi@uconn.edu>
2018-03-07 06:04:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 function dwarfRollCall(dwarves) {
+  var stringDwarves = [];
+  var emptyArr = [];
+  for (var i = 0; i < dwarves.length; i++) {
+     stringDwarves.push(i+1 + ". " + dwarves[i]);
+  }
+  for (var k = 0; k < stringDwarves.length; k++) {
+    emptyArr += (stringDwarves[k] + " ");
+  }
+  console.log(emptyArr.toString())
+  return emptyArr.toString();
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var upperCaseArray = [];
+  planeteerCalls.forEach((planeteerCall) => {
+  upperCaseArray.push(planeteerCall.toUpperCase() + "!");
+  });
+  console.log(upperCaseArray);
+  return upperCaseArray;
 }
 
 function longPlaneteerCalls(words) {
+  var counter = 0;
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].length > 4){
+      counter++;
+    }
+  }
+  if (counter === 0) {
+    return false;
+  } else {
+    counter > 0;
+    return true;
+  }
 }
 
 function findTheCheese (foods) {
+  for (var i = 0; i < foods.length; i++) {
+  if (foods[i] === "cheddar") {
+    return "cheddar";
+  }
+  }
+  return "no cheese!"
 }
